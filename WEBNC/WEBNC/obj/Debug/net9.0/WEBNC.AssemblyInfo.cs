b618b78990e3663348b5af55248148f01fc53d55
HuyPham <phamminhhuy0901tk@gmail.com>
2025-10-28 10:34:44 +0700

    add gia and alter table sanpham

--- WEBNC/WEBNC/obj/Debug/net9.0/WEBNC.AssemblyInfo.cs
+++ WEBNC/WEBNC/obj/Debug/net9.0/WEBNC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBNC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e05595df7412c0a6ed2da2ca3b9b1420dd123b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f153fe6bddb878763c8a0d7b7b3d38128f9e0e9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBNC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBNC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

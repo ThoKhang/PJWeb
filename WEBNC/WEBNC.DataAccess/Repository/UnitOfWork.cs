48298a7ff70a479f554abc9aa9df3e238d707a95
vduong2603 <vduong2603@outlook.com>
2025-11-20 12:39:54 +0700

    Fix don dat hang

--- WEBNC/WEBNC.DataAccess/Repository/UnitOfWork.cs
+++ WEBNC/WEBNC.DataAccess/Repository/UnitOfWork.cs
@@ -2,6 +2,7 @@
 using WEBNC.Data;
 using WEBNC.DataAccess.Repository;
 using WEBNC.DataAccess.Repository.IRepository;
+using WEBNC.Models;
 
 namespace Web.DataAccess.Repository
 {
@@ -12,9 +13,13 @@ namespace Web.DataAccess.Repository
         {
             _db = db;
             SanPham = new SanPhamRepository(_db);
+            DonDatHang = new DonDatHangRepository(_db);
+            ChiTietDonHang = new ChiTietDonHangRepository(_db);
+
         }
         public ISanPhamRepository SanPham { get; private set; }
-
+        public IDonDatHangRepository DonDatHang { get; private set; }
+        public IChiTietDonHangRepository ChiTietDonHang { get; private set; }
         public void save()
         {
             _db.SaveChanges();

dc1ff1b1e0bbbd3905168ad2367f401c917185a9
HuyPham <phamminhhuy0901tk@gmail.com>
2025-11-21 21:21:28 +0700

    connect of me

--- WEBNC/WEBNC/obj/Debug/net9.0/WEBNC.AssemblyInfo.cs
+++ WEBNC/WEBNC/obj/Debug/net9.0/WEBNC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBNC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e71f3cb579b79f2a22a77e1e4be0e2c5c51243c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f90566c257c53d51769878d9bf486a34e0eeae96")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBNC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBNC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

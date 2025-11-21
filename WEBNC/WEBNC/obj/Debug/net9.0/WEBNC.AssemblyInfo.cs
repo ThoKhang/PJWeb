7fb4f40ae36b89ef491da5f1b013f126bb41614d
HuyPham <phamminhhuy0901tk@gmail.com>
2025-11-22 00:18:27 +0700

    move footer to layout

--- WEBNC/WEBNC/obj/Debug/net9.0/WEBNC.AssemblyInfo.cs
+++ WEBNC/WEBNC/obj/Debug/net9.0/WEBNC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBNC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f90566c257c53d51769878d9bf486a34e0eeae96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad04a4179ce8613c3ab1ce5fc0324c1a52311793")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBNC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBNC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

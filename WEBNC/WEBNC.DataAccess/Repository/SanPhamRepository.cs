8a353a4e8c03795a13d95723d70f0ec5b59e4a9e
HuyPham <phamminhhuy0901tk@gmail.com>
2025-11-23 21:59:18 +0700

    đã add identity

--- WEBNC/WEBNC.DataAccess/Repository/SanPhamRepository.cs
+++ WEBNC/WEBNC.DataAccess/Repository/SanPhamRepository.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Web.DataAccess.Repository;
 using WEBNC.Data;
 using WEBNC.DataAccess.Repository.IRepository;
 using WEBNC.Models;

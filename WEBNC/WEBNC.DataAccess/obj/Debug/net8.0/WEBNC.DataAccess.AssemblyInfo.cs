bf3085d6cbcbece611430011c97b7a5246bdfb38
HuyPham <phamminhhuy0901tk@gmail.com>
2025-11-23 23:16:10 +0700

    update identity into db

--- WEBNC/WEBNC.DataAccess/obj/Debug/net8.0/WEBNC.DataAccess.AssemblyInfo.cs
+++ WEBNC/WEBNC.DataAccess/obj/Debug/net8.0/WEBNC.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBNC.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0c78b203fa84b3253aab4dcb647ae0600d2dd27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c74eea36932d0280be0cb745893cb841898cc9ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBNC.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBNC.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

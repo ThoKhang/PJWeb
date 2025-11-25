f28eb88114c225ec011599ec0705a9723b815181
HuyPham <phamminhhuy0901tk@gmail.com>
2025-11-25 20:42:51 +0700

    models tinh huyen phuongxa

--- WEBNC/WEBNC.Models/Huyen.cs
+++ WEBNC/WEBNC.Models/Huyen.cs
@@ -2,20 +2,28 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WEBNC.Models
 {
-    public class Tinh
+    public class Huyen
     {
         [Key]
+        public string idHuyen { get; set; }
+
         public string idTinh { get; set; }
 
-        public string tenTinh { get; set; }
+        [ForeignKey("idTinh")]
+        [ValidateNever]
+        public Tinh Tinh { get; set; }
+
+        public string tenHuyen { get; set; }
 
         [ValidateNever]
-        public ICollection<Huyen> Huyens { get; set; }
+        public ICollection<XaPhuong> XaPhuongs { get; set; }
     }
 }
+
---
+++ WEBNC/WEBNC.Models/XaPhuong.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WEBNC.Models
+{
+    public class XaPhuong
+    {
+        public string idXaPhuong { get; set; }
+        public string idHuyen { get; set; }
+        public string tenXaPhuong { get; set; }
+        public virtual Huyen Huyen { get; set; }
+    }
+}
+

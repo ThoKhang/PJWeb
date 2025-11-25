b09c414723e23aecdc31fa05e55c5ff57e09a590
HuyPham <phamminhhuy0901tk@gmail.com>
2025-11-25 20:34:33 +0700

    add phuong and chitietgiohang and tinh

---
+++ WEBNC/WEBNC.Models/Tinh.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WEBNC.Models
+{
+    public class Tinh
+    {
+        [Key]
+        public string idTinh { get; set; }
+        public string tenTinh { get; set; }
+
+    }
+}

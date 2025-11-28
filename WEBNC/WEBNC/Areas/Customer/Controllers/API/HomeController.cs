1499e82331f1efa25e76efd04bd048e37b24a2b1
HuyPham <phamminhhuy0901tk@gmail.com>
2025-11-28 21:00:46 +0700

    update-project

--- WEBNC/WEBNC/Areas/Customer/Controllers/API/HomeController.cs
+++ WEBNC/WEBNC/Areas/Customer/Controllers/API/HomeController.cs
@@ -5,48 +5,42 @@ using System.Security.Claims;
 using WEBNC.DataAccess.Repository.IRepository;
 using WEBNC.Models;
 
-namespace WEBNC.Areas.Customer.Controllers
+namespace WEBNC.Areas.Customer.Controllers.API
 {
-    [Area("Customer")]
-    public class HomeController : Controller
+    [ApiController]
+    [Route("api/customer/products")]
+    public class HomeController : ControllerBase
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger,IUnitOfWork unitOfWork)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
             _unitOfWork = unitOfWork;
         }
-        public IActionResult Index()
-        {
-            return View();
-        }
+
         [HttpGet]
         public IActionResult GetAll()
         {
-            IEnumerable<SanPham> sanPhamList = _unitOfWork.SanPham.GetAll(includeProperties:"LoaiSanPham");
-            return Json(new {data=sanPhamList});
+            IEnumerable<SanPham> sanPhamList = _unitOfWork.SanPham.GetAll(includeProperties: "LoaiSanPham");
+            return Ok(new { data = sanPhamList });
         }
-        [HttpGet]
-        public IActionResult SanPham(string? id)
+        [HttpGet("{id}")]
+        public IActionResult SanPhamById(string? id)
         {
             if (id == null || id == "")
                 return BadRequest();
             var sanPham = _unitOfWork.SanPham.GetFirstOrDefault(item => item.idSanPham == id);
             if (sanPham == null)
                 return NotFound();
-            return Json(new {data=sanPham});
+            return Ok(new { data = sanPham });
         }
-        public IActionResult Details()
-        {
-            return View();
-        }
-        [HttpGet]
+        [HttpGet("top")]
         public IActionResult TopSanPhamBanChay()
         {
             var topSanPham = _unitOfWork.SanPham.LayTop10SanPhamBanChay();
-            return Json(new { data = topSanPham });
+            return Ok(new { data = topSanPham });
         }
         [HttpPost("giohang/tang")]
         [ValidateAntiForgeryToken]
@@ -135,15 +129,5 @@ namespace WEBNC.Areas.Customer.Controllers
             });
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }

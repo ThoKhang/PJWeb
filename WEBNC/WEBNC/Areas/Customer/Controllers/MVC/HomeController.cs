d7b3634b421289f995fad7701fbee0678a3abd08
HuyPham <phamminhhuy0901tk@gmail.com>
2025-11-28 21:00:46 +0700

    update-project

---
+++ WEBNC/WEBNC/Areas/Customer/Controllers/MVC/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Claims;
+using WEBNC.DataAccess.Repository.IRepository;
+using WEBNC.Models;
+
+namespace WEBNC.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}

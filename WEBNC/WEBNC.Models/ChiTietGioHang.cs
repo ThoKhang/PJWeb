c82a0c5e745dc5aaaa0e0da27ccdaa19904d983f
HuyPham <phamminhhuy0901tk@gmail.com>
2025-11-29 01:06:33 +0700

    add update

--- WEBNC/WEBNC.Models/ChiTietGioHang.cs
+++ WEBNC/WEBNC.Models/ChiTietGioHang.cs
@@ -1,19 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using WEBNC.Models;
 
-public class ChiTietGioHang
+namespace WEBNC.Models
 {
-    [Key]
-    public string idChiTietGioHang { get; set; }
+    public class ChiTietGioHang
+    {
+        [Key]
+        public string idChiTietGioHang { get; set; }
 
-    public string idNguoiDung { get; set; }
-    [ForeignKey("idNguoiDung")]
-    public ApplicationUser user { get; set; }
+        public string idNguoiDung { get; set; }
+        [ForeignKey("idNguoiDung")]
+        public ApplicationUser user { get; set; }
 
-    public string idSanPham { get; set; }
-    [ForeignKey("idSanPham")]
-    public SanPham SanPham { get; set; }
+        public string idSanPham { get; set; }
+        [ForeignKey("idSanPham")]
+        public SanPham SanPham { get; set; }
 
-    public int soLuongTrongGio { get; set; }
+        public int soLuongTrongGio { get; set; }
+    }
 }
---
+++ WEBNC/WEBNC/Areas/Customer/Controllers/API/Cart.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using WEBNC.DataAccess.Repository.IRepository;
+
+namespace WEBNC.Areas.Customer.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize] // bắt buộc đăng nhập mới xem giỏ hàng
+    public class Cart : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public Cart(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult GetUserCart()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (claim == null)
+            {
+                return Unauthorized(new { message = "Bạn chưa đăng nhập" });
+            }
+
+            var chiTietGioHang = _unitOfWork.chiTietGioHang
+                .GetAll(u => u.idNguoiDung == claim.Value, includeProperties: "SanPham");
+
+            return Ok(new { data = chiTietGioHang });
+        }
+    }
+}

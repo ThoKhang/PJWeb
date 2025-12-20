676561023ddbac92ba6a2fd2649c356722c0db68
ThoKhang <khangheheqt@gmail.com>
2025-12-21 00:58:53 +0700

    frontend sanpham

---
+++ WEBNC/WEBNC/Areas/Admin/Controllers/MVC/SanPhamController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEBNC.Areas.Admin.Controllers.MVC
+{
+    public class SanPhamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

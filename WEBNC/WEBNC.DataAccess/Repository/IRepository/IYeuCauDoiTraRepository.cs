a9c30833722b5cf0c84f91dd2e2a3abb946b5b5e
vduong2603 <vduong2603@outlook.com>
2025-12-21 19:35:07 +0700

    Chuc nang yeu cau doi tra

---
+++ WEBNC/WEBNC.DataAccess/Repository/IRepository/IYeuCauDoiTraRepository.cs
@@ -0,0 +1,9 @@
+﻿using WEBNC.Models;
+
+namespace WEBNC.DataAccess.Repository.IRepository
+{
+    public interface IYeuCauDoiTraRepository : IRepository<YeuCauDoiTra>
+    {
+        void Update(YeuCauDoiTra obj);
+    }
+}

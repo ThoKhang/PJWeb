d97be74ac22e8a437ac285567e7e394ebf0c184f
HuyPham <phamminhhuy0901tk@gmail.com>
2025-12-22 09:36:01 +0700

    add admin

---
+++ WEBNC/WEBNC/Areas/Admin/Controllers/API/LoaiSanPham.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WEBNC.DataAccess.Repository.IRepository;
+
+namespace WEBNC.Areas.Admin.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoaiSanPham : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public LoaiSanPham(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        [HttpGet("loaiSanPham")]
+        public IActionResult GetAll()
+        {
+            IEnumerable<Models.LoaiSanPham> objList = _unitOfWork.LoaiSanPham.GetAll();
+            if(objList == null || !objList.Any())
+            {
+                return NotFound();
+            }
+            return Ok(objList);
+        }
+    }
+}
---
+++ WEBNC/WEBNC/Areas/Admin/Controllers/MVC/SanPhamController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WEBNC.DataAccess.Repository.IRepository;
+
+[Area("Admin")]
+public class SanPhamController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public SanPhamController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public IActionResult Index()
+    {
+        var list = _unitOfWork.SanPham.GetAll(includeProperties: "LoaiSanPham");
+
+        return View(list);
+    }
+}

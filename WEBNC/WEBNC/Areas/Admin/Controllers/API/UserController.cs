0763806e347891e8b0f905e8e33d0b68bbf20aab
HuyPham <phamminhhuy0901tk@gmail.com>
2025-12-22 11:08:51 +0700

    add user

---
+++ WEBNC/WEBNC/Areas/Admin/Controllers/API/UserController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WEBNC.Models;
+
+namespace WEBNC.Areas.Admin.Controllers.API
+{
+    [Area("Admin")]
+    [Route("api/admin/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var users = await _userManager.Users.Include(u => u.xaPhuong).ToListAsync();
+            var result = new List<object>();
+            foreach (var u in users)
+            {
+                var roles = await _userManager.GetRolesAsync(u);
+
+                result.Add(new
+                {
+                    id = u.Id,
+                    userName = u.UserName,
+                    email = u.Email,
+                    phone = u.PhoneNumber,
+                    diaChi = $"{u.soNha}, {u.xaPhuong?.tenXaPhuong}",
+                    role = roles.FirstOrDefault(),
+                    emailConfirmed = u.EmailConfirmed,
+                    isOtpVerified = u.IsOtpVerified
+                });
+            }
+
+            return Ok(result);
+        }
+    }
+}

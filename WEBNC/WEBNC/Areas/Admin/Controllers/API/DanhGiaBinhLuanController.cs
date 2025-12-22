82ecae865b65b660d2eed8b5879752b6fc710d8b
HuyPham <phamminhhuy0901tk@gmail.com>
2025-12-22 23:10:48 +0700

    add binhluan admin

---
+++ WEBNC/WEBNC/Areas/Admin/Controllers/API/DanhGiaBinhLuanController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WEBNC.DataAccess.Repository.IRepository;
+
+namespace WEBNC.Areas.Admin.Controllers.API
+{
+    [Route("api/admin/danhgia")]
+    [ApiController]
+    public class DanhGiaBinhLuanController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public DanhGiaBinhLuanController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var danhGia = _unitOfWork.DanhGia.GetAll(includeProperties: "SanPham,User,DonDatHang");
+            return Ok(danhGia);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var dg = _unitOfWork.DanhGia.GetFirstOrDefault(x => x.idDanhGia == id);
+            if (dg == null)
+                return NotFound(new { message = "Không tìm thấy đánh giá" });
+            _unitOfWork.DanhGia.Remove(dg);
+            _unitOfWork.Save();
+            return Ok(new { message = "Đã xoá đánh giá" });
+        }
+    }
+}
---
+++ WEBNC/WEBNC/Areas/Admin/Controllers/MVC/DanhGiaBinhLuanController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEBNC.Areas.Admin.Controllers.MVC
+{
+    [Area("Admin")]
+    public class DanhGiaBinhLuanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

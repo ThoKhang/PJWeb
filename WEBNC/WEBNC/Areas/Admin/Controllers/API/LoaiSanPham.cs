033e1178868baf13eef65fa9dc5d7e8f50b3e232
HuyPham <phamminhhuy0901tk@gmail.com>
2025-12-22 10:23:50 +0700

    add admin

--- WEBNC/WEBNC/Areas/Admin/Controllers/API/LoaiSanPham.cs
+++ WEBNC/WEBNC/Areas/Admin/Controllers/API/LoaiSanPham.cs
@@ -1,27 +1,41 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using WEBNC.DataAccess.Repository.IRepository;
 
-namespace WEBNC.Areas.Admin.Controllers.API
+[Area("Admin")]
+[Route("api/admin/loaisanpham")]
+[ApiController]
+public class LoaiSanPhamController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class LoaiSanPham : ControllerBase
+    private readonly IUnitOfWork _unitOfWork;
+
+    public LoaiSanPhamController(IUnitOfWork unitOfWork)
     {
-        private readonly IUnitOfWork _unitOfWork;
-        public LoaiSanPham(IUnitOfWork unitOfWork)
+        _unitOfWork = unitOfWork;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var objList = _unitOfWork.LoaiSanPham.GetAll();
+        if (objList == null || !objList.Any())
+            return NotFound();
+        return Ok(objList);
+    }
+    [HttpDelete("{id}")]
+    public IActionResult Delete(string id)
+    {
+        var obj = _unitOfWork.LoaiSanPham.GetFirstOrDefault(x => x.idLoaiSanPham == id);
+        if (obj == null)
+            return NotFound(new { message = "Không tồn tại" });
+        try
         {
-            _unitOfWork = unitOfWork;
+            _unitOfWork.LoaiSanPham.Remove(obj);
+            _unitOfWork.Save();
+            return Ok(new { success = true });
         }
-        [HttpGet("loaiSanPham")]
-        public IActionResult GetAll()
+        catch
         {
-            IEnumerable<Models.LoaiSanPham> objList = _unitOfWork.LoaiSanPham.GetAll();
-            if(objList == null || !objList.Any())
-            {
-                return NotFound();
-            }
-            return Ok(objList);
+            return BadRequest(new { message = "Loại sản phẩm đang được sử dụng" });
         }
     }
 }
